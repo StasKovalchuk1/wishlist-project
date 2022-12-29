7520ce32da348ba26d8b23009869a3dc697dc44b
StasKovalchuk1 <64376198+StasKovalchuk1@users.noreply.github.com>
2022-12-29 18:22:47 +0100

    last changes 29.12

--- main/cancelReservation.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "connect.php";
-
-$id = $_GET['id'];
-mysqli_query($connect, "UPDATE `wishes` SET `reservation` = NULL WHERE `wishes`.`id` = '$id'");
-header("Location: ../searchPage.php");
-?>
--- main/destroySession.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['session'] = 'inactive';
-unset($_COOKIE["username"]);
-header('Location: ../login.php');
-?>
