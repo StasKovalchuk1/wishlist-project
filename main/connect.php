360a69c16aebf34a7281a139bcd1415ea7a61ec3
StasKovalchuk1 <64376198+StasKovalchuk1@users.noreply.github.com>
2023-08-12 15:26:13 +0200

    rewriting wishlist app with java spring

--- main/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $connect = mysqli_connect('localhost', 'kovalst1', 'webove aplikace', 'kovalst1');
-    if (!$connect){
-        die("ERROR");
-    }
--- main/destroySession.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['session'] = 'inactive';
-header('Location: ../login.php');
-?>
